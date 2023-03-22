e6377bea1052b42cfc7783d76c2c7438f72c9c9a
Pandey <vedant.pandey@arcadis.com>
2023-03-22 16:33:06 +0530

    Changes in contact.js

--- test-app/src/components/contact.js
+++ test-app/src/components/contact.js
@@ -1,27 +1,79 @@
 import React from "react";
+import { useState } from "react";
 // import 'semantic-ui-css/semantic.min.css';
-import { Button, Checkbox, Form} from "semantic-ui-react";
+import { Button, Checkbox, Form, Container} from "semantic-ui-react";
 
 
-class AddContact extends React.Component{
-    render() {
-        return (
-            <div className="ui-main">
-                <h2>Add Contact</h2>
-                <form className="ui form">
-                    <div className="field">
-                        <lable>Name</lable>
-                        <input type="text" name = "name" placeholder="Name"/>
-                    </div>
-                    <div className="field">
-                        <lable>Name</lable>
-                        <input type="text" name = "name" placeholder="Name"/>
-                    </div>
-                    <button type='submit' className="ui button">Add</button>
-                </form>
-            </div>
-        )
+function AddContact() {
+
+    // const myStyle = {
+    //     color: "white",
+    //     backgroundColor: "White",
+    //     padding: "5px",
+    //     fontFamily: "Sans-Serif",
+    //     box-sizing:"border-box";
+    //   };
+
+    const [fname, setFirstName] = useState("");
+    const [lname, setLastName] = useState("");
+    const [mail, setMail] = useState("");
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      const name_dict = {
+        "First Name": fname,
+        "Last Name": lname,
+        "E-mail":mail
+      }
     }
-}
+  
+    return (
+        <Container  style={{ margin: 20 }}>
+      <Form onSubmit={handleSubmit}>
+        <Form.Field>
+        <label>First Name :  </label>
+          <input 
+            type="text" 
+            value={fname}
+            onChange={(e) => setFirstName(e.target.value)}
+            placeholder='First Name'
+            // style={myStyle}
+          />
+        {/* </label> */}
+        </Form.Field>
+        <Form.Field>
+        <label>Last Name :  </label>
+          <input 
+            type="text" 
+            value={lname}
+            onChange={(e) => setLastName(e.target.value)}
+            placeholder='Last Name'
+            // style={myStyle}
+          />
+        {/* </label> */}
+        </Form.Field>
+
+        <Form.Field>
+        <label>Email : </label>
+          <input 
+            type="text" 
+            value={mail}
+            onChange={(e) => setMail(e.target.value)}
+            placeholder='E-mail'
+          />
+    
+        </Form.Field>
+
+        <Checkbox label='I agree to the Terms and Conditions' />
+
+            
+        <Form.Field>
+        <Button type="submit">Add </Button>
+        </Form.Field>
+      </Form>
+
+      </Container>
+    )
+  }
 
 export default AddContact;
